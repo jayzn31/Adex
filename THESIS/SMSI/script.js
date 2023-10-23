01555acccd840db6d0e413d96153502812f5c4e0
jayzn31 <60603791+jayzn31@users.noreply.github.com>
2023-10-23 16:57:35 +0800

    Delete THESIS directory

--- THESIS/SMSI/script.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-  fetch('/api/jobs', {
-    method: 'POST',
-    body: data,
-  });
-});
